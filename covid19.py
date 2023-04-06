ffa01addac6797f1c002ed97d3d6e66c31a49ed6
sushmitha6145 <128017237+sushmitha6145@users.noreply.github.com>
2023-04-06 19:49:30 +0530

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -37,9 +37,9 @@ if uploaded_file is not None:
     q6a = st.sidebar.checkbox("Q6-A: Sort the entire data wrt No. of Confirmed cases in ascending order")
     q6b = st.sidebar.checkbox("Q6-B: Sort the entire data wrt No. of Recovered cases in descending order")
     q7 = st.sidebar.checkbox("Q7: Check if the patient is likely to have COVID-19 based on symptoms") 
-    q8 = st.sidebar.checkbox("Q7: What is the average number of confirmed cases per day in each region?")
-    q9 = st.sidebar.checkbox("Q7: Which countries have a death rate of over 10%?")
-    q10 = st.sidebar.checkbox("Q8: Which countries have the highest mortality rates (number of deaths / number of confirmed cases)?")
+    q8 = st.sidebar.checkbox("Q8: What is the average number of confirmed cases per day in each region?")
+    q9 = st.sidebar.checkbox("Q9: Which countries have a death rate of over 10%?")
+    q10 = st.sidebar.checkbox("Q10: Which countries have the highest mortality rates (number of deaths / number of confirmed cases)?")
 
     # Question 1
     if q1:
@@ -99,7 +99,7 @@ if uploaded_file is not None:
           st.write("Based on the symptoms, the patient is unlikely to have COVID-19.")
     #question 8
     if q8:
-       st.subheader("Q7: What is the average number of confirmed cases per day in each region?")
+       st.subheader("Q8: What is the average number of confirmed cases per day in each region?")
        data['Date'] = pd.to_datetime(data['Date'])
        data['Day'] = data['Date'].dt.date
        data_grouped = data.groupby(['Region', 'Day']).sum().reset_index()
@@ -107,13 +107,13 @@ if uploaded_file is not None:
        st.write(avg_confirmed_cases)
     # Question 9
     if q9:
-       st.subheader("Q7: Which countries have a death rate of over 10%?")
+       st.subheader("Q9: Which countries have a death rate of over 10%?")
        data['Death Rate'] = (data['Deaths'] / data['Confirmed']) * 100
        high_death_rate = data[data['Death Rate'] > 10]['Country/Region']
        st.write(high_death_rate.unique())
     # Question 10
     if q10:
-       st.subheader("Q8: Which countries have the highest mortality rates (number of deaths / number of confirmed cases)?")
+       st.subheader("Q10: Which countries have the highest mortality rates (number of deaths / number of confirmed cases)?")
        # Calculate mortality rate
        data['Mortality_Rate'] = data['Deaths'] / data['Confirmed']
        # Group data by country and sort by mortality rate
