04fe0af6509a9cc7f13f5b302c3ce1e7acb9fb05
sushmitha6145 <128017237+sushmitha6145@users.noreply.github.com>
2023-04-07 12:32:49 +0530

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -96,6 +96,42 @@ if uploaded_file is not None:
                 avg_confirmed_cases = data_grouped.groupby('Region')['Confirmed'].mean()
                 st.write(avg_confirmed_cases)
         
+    # Question 10
+
+if st.checkbox("Q10: Which region have the highest mortality rates (number of deaths / number of confirmed cases)?"):
+    # Calculate mortality rate
+    data['Mortality_Rate'] = data['Deaths'] / data['Confirmed']
+    # Group data by region and sort by mortality rate
+    mortality_by_Region = data.groupby('Region')['Mortality_Rate'].max().sort_values(ascending=False)
+    st.write(mortality_by_Region)
+# Question 7
+
+if st.checkbox("Q7: Check if the patient is likely to have COVID-19 based on symptoms"):
+    # Create checkboxes for symptoms
+    fever = st.checkbox("Fever")
+    cough = st.checkbox("Dry Cough")
+    tiredness = st.checkbox("Tiredness")
+    breathing_difficulty = st.checkbox("Difficulty in Breathing")
+    sore_throat = st.checkbox("Sore Throat")
+    body_aches = st.checkbox("Body Aches")
+    loss_of_smell_or_taste = st.checkbox("Loss of Smell or Taste")
+
+    # Check if any of the symptoms are present
+    if fever or cough or tiredness or breathing_difficulty or sore_throat or body_aches or loss_of_smell_or_taste:
+        st.write("Based on the symptoms, the patient may have COVID-19.")
+    else:
+        st.write("Based on the symptoms, the patient is unlikely to have COVID-19.")
+
+# Question 8
+
+if st.checkbox("Q8: What is the average number of confirmed cases per day in each region?"):
+    data['Date'] = pd.to_datetime(data['Date'])
+    #data['Day'] = data['Date'].dt.date
+    data_grouped = data.groupby(['Region'].sum().reset_index())
+    avg_confirmed_cases = data_grouped.groupby('Region')['Confirmed'].mean()
+    st.write(avg_confirmed_cases)
+
+        
     # Question 9  
     
     if st.checkbox("Q9: Which region have a death rate of over 10%?"):
