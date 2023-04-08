0b194f7397633c8f97397e5969aecddd0e5f43b3
sushmitha6145 <128017237+sushmitha6145@users.noreply.github.com>
2023-04-08 14:27:36 +0530

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -110,12 +110,12 @@ if uploaded_file is not None:
            st.write(mortality_by_Region)
     
     # Question 11
-    if st.checkbox("Q11: Show a line graph showing the total confirmed cases for each region (top 20)"):
+    if st.checkbox("Q11: Show a line graph showing the total confirmed cases for each region (top 10)"):
            region_confirmed = data.groupby("Region")["Confirmed"].sum().reset_index()
-           region_confirmed = region_confirmed.sort_values("Confirmed", ascending=False).head(20)
+           region_confirmed = region_confirmed.sort_values("Confirmed", ascending=False).head(10)
            fig, ax = plt.subplots()
            sns.lineplot(x="Region", y="Confirmed", data=region_confirmed, ax=ax)
-           ax.set_title("Total Confirmed Cases by Region (Top 20)")
+           ax.set_title("Total Confirmed Cases by Region (Top 10)")
            ax.set_xlabel("Region")
            ax.set_ylabel("Confirmed Cases")
            st.pyplot(fig)
